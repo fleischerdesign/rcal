ae9b9b1904f4eabe03532a7830c2e602f536a888
Philipp Fleischer <philipp@fleischer.design>
2026-02-24 10:09:51 +0100

    feat: upgrade to v0.2.0 with modulo, power, functions, variables and sequential execution

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use std::collections::HashMap;
 use std::io::{self, Write};
 
 #[derive(Debug, Clone, PartialEq)]
 enum Token {
     Number(f64),
+    Identifier(String),
     Plus,
     Minus,
     Multiply,
     Divide,
+    Modulo,
+    Power,
+    Assign,
+    Semicolon,
     LParen,
     RParen,
     EOF,
@@ -14,10 +20,17 @@ enum Token {
 
 enum Expr {
     Number(f64),
+    Variable(String),
+    Assign(String, Box<Expr>),
+    Function(String, Box<Expr>),
     Add(Box<Expr>, Box<Expr>),
     Subtract(Box<Expr>, Box<Expr>),
     Multiply(Box<Expr>, Box<Expr>),
     Divide(Box<Expr>, Box<Expr>),
+    Modulo(Box<Expr>, Box<Expr>),
+    Power(Box<Expr>, Box<Expr>),
+    UnaryMinus(Box<Expr>),
+    UnaryPlus(Box<Expr>),
 }
 
 fn tokenize(input: &str) -> Result<Vec<Token>, String> {
@@ -45,6 +58,22 @@ fn tokenize(input: &str) -> Result<Vec<Token>, String> {
                 tokens.push(Token::Divide);
                 chars.next();
             }
+            '%' => {
+                tokens.push(Token::Modulo);
+                chars.next();
+            }
+            '^' => {
+                tokens.push(Token::Power);
+                chars.next();
+            }
+            '=' => {
+                tokens.push(Token::Assign);
+                chars.next();
+            }
+            ';' => {
+                tokens.push(Token::Semicolon);
+                chars.next();
+            }
             '(' => {
                 tokens.push(Token::LParen);
                 chars.next();
@@ -52,6 +81,17 @@ fn tokenize(input: &str) -> Result<Vec<Token>, String> {
             ')' => {
                 tokens.push(Token::RParen);
                 chars.next();
+            }
+            'a'..='z' | 'A'..='Z' | '_' => {
+                let mut ident = String::new();
+                while let Some(&ch) = chars.peek() {
+                    if ch.is_alphanumeric() || ch == '_' {
+                        ident.push(chars.next().unwrap());
+                    } else {
+                        break;
+                    }
+                }
+                tokens.push(Token::Identifier(ident));
             }
             '0'..='9' | '.' => {
                 let mut num_str = String::new();
@@ -87,11 +127,27 @@ impl Parser {
         self.tokens.get(self.pos).unwrap_or(&Token::EOF)
     }
 
+    fn peek(&self) -> &Token {
+        self.tokens.get(self.pos + 1).unwrap_or(&Token::EOF)
+    }
+
     fn consume(&mut self) {
         self.pos += 1;
     }
 
     fn parse_expr(&mut self) -> Result<Box<Expr>, String> {
+        if let Token::Identifier(name) = self.current().clone() {
+            if self.peek() == &Token::Assign {
+                self.consume();
+                self.consume();
+                let right = self.parse_expr()?;
+                return Ok(Box::new(Expr::Assign(name.to_lowercase(), right)));
+            }
+        }
+        self.parse_additive()
+    }
+
+    fn parse_additive(&mut self) -> Result<Box<Expr>, String> {
         let mut left = self.parse_term()?;
 
         while self.current() == &Token::Plus || self.current() == &Token::Minus {
@@ -109,23 +165,30 @@ impl Parser {
     }
 
     fn parse_term(&mut self) -> Result<Box<Expr>, String> {
-        let mut left = self.parse_factor()?;
+        let mut left = self.parse_power()?;
 
         loop {
             match self.current() {
-                Token::Multiply | Token::Divide => {
+                Token::Multiply | Token::Divide | Token::Modulo => {
                     let op = self.current().clone();
                     self.consume();
-                    let right = self.parse_factor()?;
+                    let right = self.parse_power()?;
 
-                    if op == Token::Multiply {
-                        left = Box::new(Expr::Multiply(left, right));
-                    } else {
-                        left = Box::new(Expr::Divide(left, right));
+                    match op {
+                        Token::Multiply => {
+                            left = Box::new(Expr::Multiply(left, right));
+                        }
+                        Token::Divide => {
+                            left = Box::new(Expr::Divide(left, right));
+                        }
+                        Token::Modulo => {
+                            left = Box::new(Expr::Modulo(left, right));
+                        }
+                        _ => unreachable!(),
                     }
                 }
-                Token::LParen => {
-                    let right = self.parse_factor()?;
+                Token::LParen | Token::Identifier(_) | Token::Number(_) => {
+                    let right = self.parse_power()?;
                     left = Box::new(Expr::Multiply(left, right));
                 }
                 _ => break,
@@ -134,11 +197,58 @@ impl Parser {
         Ok(left)
     }
 
+    fn parse_power(&mut self) -> Result<Box<Expr>, String> {
+        let left = self.parse_unary()?;
+
+        if self.current() == &Token::Power {
+            self.consume();
+            let right = self.parse_power()?;
+            Ok(Box::new(Expr::Power(left, right)))
+        } else {
+            Ok(left)
+        }
+    }
+
+    fn parse_unary(&mut self) -> Result<Box<Expr>, String> {
+        match self.current() {
+            Token::Minus => {
+                self.consume();
+                let expr = self.parse_unary()?;
+                Ok(Box::new(Expr::UnaryMinus(expr)))
+            }
+            Token::Plus => {
+                self.consume();
+                let expr = self.parse_unary()?;
+                Ok(Box::new(Expr::UnaryPlus(expr)))
+            }
+            _ => self.parse_factor(),
+        }
+    }
+
     fn parse_factor(&mut self) -> Result<Box<Expr>, String> {
         match self.current().clone() {
             Token::Number(n) => {
                 self.consume();
                 Ok(Box::new(Expr::Number(n)))
+            }
+            Token::Identifier(name) => {
+                self.consume();
+                let name = name.to_lowercase();
+                if self.current() == &Token::LParen {
+                    self.consume();
+                    let arg = self.parse_expr()?;
+                    if self.current() == &Token::RParen {
+                        self.consume();
+                        Ok(Box::new(Expr::Function(name, arg)))
+                    } else {
+                        Err(
+                            "SyntaxError: Lacking closing parenthesis after function argument"
+                                .to_string(),
+                        )
+                    }
+                } else {
+                    Ok(Box::new(Expr::Variable(name)))
+                }
             }
             Token::LParen => {
                 self.consume();
@@ -147,50 +257,127 @@ impl Parser {
                     self.consume();
                     Ok(expr)
                 } else {
-                    Err("SyntaxtError: Lacking closing parenthesis".to_string())
+                    Err("SyntaxError: Lacking closing parenthesis".to_string())
                 }
             }
             Token::EOF => Err("Unexpected ending of input".to_string()),
-            _ => Err("SyntaxError: Unexpexted character".to_string()),
+            _ => Err(format!(
+                "SyntaxError: Unexpected token {:?}",
+                self.current()
+            )),
         }
     }
 }
 
-fn evaluate(expr: &Expr) -> f64 {
+fn evaluate(expr: &Expr, vars: &mut HashMap<String, f64>) -> Result<f64, String> {
     match expr {
-        Expr::Number(n) => *n,
-        Expr::Add(l, r) => evaluate(l) + evaluate(r),
-        Expr::Subtract(l, r) => evaluate(l) - evaluate(r),
-        Expr::Multiply(l, r) => evaluate(l) * evaluate(r),
-        Expr::Divide(l, r) => evaluate(l) / evaluate(r),
+        Expr::Number(n) => Ok(*n),
+        Expr::Variable(name) => {
+            if name == "pi" {
+                return Ok(std::f64::consts::PI);
+            }
+            if name == "e" {
+                return Ok(std::f64::consts::E);
+            }
+            vars.get(name)
+                .copied()
+                .ok_or_else(|| format!("Math Error: Unknown variable '{}'", name))
+        }
+        Expr::Assign(name, val_expr) => {
+            let val = evaluate(val_expr, vars)?;
+            vars.insert(name.clone(), val);
+            Ok(val)
+        }
+        Expr::Function(name, arg) => {
+            let val = evaluate(arg, vars)?;
+            match name.as_str() {
+                "sin" => Ok(val.sin()),
+                "cos" => Ok(val.cos()),
+                "tan" => Ok(val.tan()),
+                "asin" => Ok(val.asin()),
+                "acos" => Ok(val.acos()),
+                "atan" => Ok(val.atan()),
+                "sqrt" => {
+                    if val < 0.0 {
+                        return Err("Math Error: Square root of negative number".to_string());
+                    }
+                    Ok(val.sqrt())
+                }
+                "abs" => Ok(val.abs()),
+                "ln" => {
+                    if val <= 0.0 {
+                        return Err(
+                            "Math Error: Natural logarithm of zero or negative number".to_string()
+                        );
+                    }
+                    Ok(val.ln())
+                }
+                "log" => {
+                    if val <= 0.0 {
+                        return Err("Math Error: Logarithm of zero or negative number".to_string());
+                    }
+                    Ok(val.log10())
+                }
+                _ => Err(format!("Math Error: Unknown function '{}'", name)),
+            }
+        }
+        Expr::Add(l, r) => Ok(evaluate(l, vars)? + evaluate(r, vars)?),
+        Expr::Subtract(l, r) => Ok(evaluate(l, vars)? - evaluate(r, vars)?),
+        Expr::Multiply(l, r) => Ok(evaluate(l, vars)? * evaluate(r, vars)?),
+        Expr::Divide(l, r) => {
+            let left = evaluate(l, vars)?;
+            let right = evaluate(r, vars)?;
+            if right == 0.0 {
+                return Err("Math Error: Division by zero".to_string());
+            }
+            let res = left / right;
+            if res.is_infinite() {
+                return Err("Math Error: Overflow".to_string());
+            }
+            Ok(res)
+        }
+        Expr::Modulo(l, r) => {
+            let left = evaluate(l, vars)?;
+            let right = evaluate(r, vars)?;
+            if right == 0.0 {
+                return Err("Math Error: Modulo by zero".to_string());
+            }
+            Ok(left % right)
+        }
+        Expr::Power(l, r) => {
+            let left = evaluate(l, vars)?;
+            let right = evaluate(r, vars)?;
+            let res = left.powf(right);
+            if res.is_infinite() {
+                return Err("Math Error: Overflow (Result too large)".to_string());
+            }
+            if res.is_nan() {
+                return Err(
+                    "Math Error: Invalid operation (e.g. negative base with fractional exponent)"
+                        .to_string(),
+                );
+            }
+            Ok(res)
+        }
+        Expr::UnaryMinus(e) => Ok(-evaluate(e, vars)?),
+        Expr::UnaryPlus(e) => evaluate(e, vars),
     }
 }
 
-fn main() {
-    println!("rcal v0.1");
-    println!("please provide a mathematical input or type 'exit'\n");
+fn process_input(input: &str, vars: &mut HashMap<String, f64>) {
+    let parts: Vec<&str> = input.split(';').collect();
 
-    loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let input = input.trim();
-
-        if input.eq_ignore_ascii_case("exit") || input.eq_ignore_ascii_case("quit") {
-            println!("Bye!");
-            break;
-        }
-        if input.is_empty() {
+    for part in parts {
+        let trimmed = part.trim();
+        if trimmed.is_empty() {
             continue;
         }
 
-        let tokens = match tokenize(input) {
+        let tokens = match tokenize(trimmed) {
             Ok(t) => t,
             Err(e) => {
                 println!("{}\n", e);
-                continue;
+                return;
             }
         };
 
@@ -199,19 +386,63 @@ fn main() {
         match parser.parse_expr() {
             Ok(ast) => {
                 if parser.current() != &Token::EOF {
-                    println!("SyntaxError: Unexpected character on end of expression");
-                    continue;
+                    println!("SyntaxError: Unexpected character on end of expression\n");
+                    return;
                 }
 
-                let result = evaluate(&ast);
-                if result.is_infinite() || result.is_nan() {
-                    println!("Error: Dividing by Null is not allowed!");
-                } else {
-                    println!("= {}\n", result);
+                match evaluate(&ast, vars) {
+                    Ok(result) => {
+                        let normalized = if result == 0.0 { 0.0 } else { result };
+                        vars.insert("ans".to_string(), normalized);
+
+                        if !matches!(*ast, Expr::Assign(_, _)) {
+                            println!("= {}\n", normalized);
+                        }
+                    }
+                    Err(e) => println!("{}\n", e),
                 }
             }
             Err(e) => {
                 println!("{}\n", e);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut vars = HashMap::new();
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() > 1 {
+        let input = args[1..].join(" ");
+        process_input(&input, &mut vars);
+        return;
+    }
+
+    println!("rcal v{}", env!("CARGO_PKG_VERSION"));
+    println!("please provide a mathematical input or type 'exit'\n");
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(0) => {
+                println!("Bye!");
+                break;
+            }
+            Ok(_) => {
+                let trimmed = input.trim();
+                if trimmed.eq_ignore_ascii_case("exit") || trimmed.eq_ignore_ascii_case("quit") {
+                    println!("Bye!");
+                    break;
+                }
+                process_input(trimmed, &mut vars);
+            }
+            Err(e) => {
+                eprintln!("Error reading input: {}", e);
+                break;
             }
         }
     }
